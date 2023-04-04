2c2d64a942904eb1c2c3da68599afe5103a90571
Silk Rose <silkrose@love-tolerance.com>
2023-04-04 15:36:19 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# My-Little-Pony-Train-Sound
+ Factorio mod to change the sound of trains.
---
+++ data-final-fixes.lua
@@ -0,0 +1,8 @@
+data.raw["locomotive"]["locomotive"].working_sound =
+    {
+      sound =
+      {
+        filename = "__mlp-train-sound__/sound/train.ogg",
+        volume = 1.0
+      }
+    }
